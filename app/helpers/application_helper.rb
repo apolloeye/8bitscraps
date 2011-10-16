d574b0466f240b2a7d1b7a54b2d988c503aa2606
Graham Hadgraft <graham@avunit.com>
2011-10-16 19:26:36 +0100

    added twitter updates

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,7 @@ module ApplicationHelper
   require 'rubygems'
   require 'simple-rss'
   require 'open-uri'
+  require 'twitter'
   
   def get_github_commits
     feed = SimpleRSS.parse open('https://github.com/bitscraps.atom')
@@ -15,7 +16,9 @@ module ApplicationHelper
           @content.gsub!(/\&lt;.*?\&gt;/, "")
           @content.gsub!(/.*? committed .*?\n/, "<br/>-")
           @content.gsub!(/View comparison for these .*? commits .*/, "")
-        
+          @content =~ /(.*?) more commits.*/
+          @content.gsub!(/(.*?) more commits.*/, "<br/>- #{$1} more commits")
+          
           @text += "<div class=commit>#{@content.sub(/^<br\/>/, "")}</div>"
           @text += "<div class='committer'>#{entry.title.gsub(/^.*? /, "").gsub(/bitscraps\//, "")}<br/>on #{entry.published.to_s.gsub(/ ..\:.*/, "")}</div>"
          @commits += 1
@@ -24,5 +27,19 @@ module ApplicationHelper
      
     end  
     @return = "<div class=github-commits>#{@text}</div>"
+  end
+  
+  def get_twitter_updates
+    @return = '<div class="twitter-update"><div class="update">'+Twitter.user_timeline("8bitscraps").first.text+'</div>'
+    @return += '<div class="date">'+Twitter.user_timeline("8bitscraps").first.created_at.gsub!(/..:..:...*/, "")+'</div></div>'
+    @return += '<div class="twitter-update"><div class="update">'+Twitter.user_timeline("8bitscraps").second.text+'</div>'
+    @return += '<div class="date">'+Twitter.user_timeline("8bitscraps").second.created_at.gsub!(/..:..:...*/, "")+'</div></div>'
+    @return += '<div class="twitter-update"><div class="update">'+Twitter.user_timeline("8bitscraps").third.text+'</div>'
+    @return += '<div class="date">'+Twitter.user_timeline("8bitscraps").third.created_at.gsub!(/..:..:...*/, "")+'</div></div>'
+    @return += '<div class="twitter-update"><div class="update">'+Twitter.user_timeline("8bitscraps").fourth.text+'</div>'
+    @return += '<div class="date">'+Twitter.user_timeline("8bitscraps").fourth.created_at.gsub!(/..:..:...*/, "")+'</div></div>'
+    @return += '<div class="twitter-update"><div class="update">'+Twitter.user_timeline("8bitscraps").fifth.text+'</div>'
+    @return += '<div class="date">'+Twitter.user_timeline("8bitscraps").fifth.created_at.gsub!(/..:..:...*/, "")+'</div></div>'
+   
   end
 end
