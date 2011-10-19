e267fb44ae9b34a0e0185880e542251827c0d3f9
Graham Hadgraft <graham@avunit.com>
2011-10-19 09:47:43 +0100

    New blog style applied

--- public/blog/wp-content/themes/twentyten/header.php
+++ public/blog/wp-content/themes/twentyten/header.php
@@ -8,83 +8,123 @@
  * @subpackage Twenty_Ten
  * @since Twenty Ten 1.0
  */
-?><!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-<meta charset="<?php bloginfo( 'charset' ); ?>" />
-<title><?php
-	/*
-	 * Print the <title> tag based on what is being viewed.
-	 */
-	global $page, $paged;
-
-	wp_title( '|', true, 'right' );
-
-	// Add the blog name.
-	bloginfo( 'name' );
-
-	// Add the blog description for the home/front page.
-	$site_description = get_bloginfo( 'description', 'display' );
-	if ( $site_description && ( is_home() || is_front_page() ) )
-		echo " | $site_description";
-
-	// Add a page number if necessary:
-	if ( $paged >= 2 || $page >= 2 )
-		echo ' | ' . sprintf( __( 'Page %s', 'twentyten' ), max( $paged, $page ) );
-
-	?></title>
-<link rel="profile" href="http://gmpg.org/xfn/11" />
-<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-<?php
-	/* We add some JavaScript to pages with the comment form
-	 * to support sites with threaded comments (when in use).
-	 */
-	if ( is_singular() && get_option( 'thread_comments' ) )
-		wp_enqueue_script( 'comment-reply' );
-
-	/* Always have wp_head() just before the closing </head>
-	 * tag of your theme, or you will break many plugins, which
-	 * generally use this hook to add elements to <head> such
-	 * as styles, scripts, and meta tags.
-	 */
-	wp_head();
 ?>
-</head>
 
-<body <?php body_class(); ?>>
-<div id="wrapper" class="hfeed">
-	<div id="header">
-		<div id="masthead">
-			<div id="branding" role="banner">
-				<?php $heading_tag = ( is_home() || is_front_page() ) ? 'h1' : 'div'; ?>
-				<<?php echo $heading_tag; ?> id="site-title">
-					<span>
-						<a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
-					</span>
-				</<?php echo $heading_tag; ?>>
-				<div id="site-description"><?php bloginfo( 'description' ); ?></div>
 
-				<?php
-					// Check if this is a post or page, if it has a thumbnail, and if it's a big one
-					if ( is_singular() && current_theme_supports( 'post-thumbnails' ) &&
-							has_post_thumbnail( $post->ID ) &&
-							( /* $src, $width, $height */ $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'post-thumbnail' ) ) &&
-							$image[1] >= HEADER_IMAGE_WIDTH ) :
-						// Houston, we have a new header image!
-						echo get_the_post_thumbnail( $post->ID );
-					elseif ( get_header_image() ) : ?>
-						<img src="<?php header_image(); ?>" width="<?php echo HEADER_IMAGE_WIDTH; ?>" height="<?php echo HEADER_IMAGE_HEIGHT; ?>" alt="" />
-					<?php endif; ?>
-			</div><!-- #branding -->
 
-			<div id="access" role="navigation">
-			  <?php /*  Allow screen readers / text browsers to skip the navigation menu and get right to the good stuff */ ?>
-				<div class="skip-link screen-reader-text"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentyten' ); ?>"><?php _e( 'Skip to content', 'twentyten' ); ?></a></div>
-				<?php /* Our navigation menu.  If one isn't filled out, wp_nav_menu falls back to wp_page_menu.  The menu assiged to the primary position is the one used.  If none is assigned, the menu with the lowest ID is used.  */ ?>
-				<?php wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'primary' ) ); ?>
-			</div><!-- #access -->
-		</div><!-- #masthead -->
-	</div><!-- #header -->
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		<!DOCTYPE html>
+		<html <?php language_attributes(); ?>>
+		<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>" />
+		<title><?php
+			/*
+			 * Print the <title> tag based on what is being viewed.
+			 */
+			global $page, $paged;
 
-	<div id="main">
+			wp_title( '|', true, 'right' );
+
+			// Add the blog name.
+			bloginfo( 'name' );
+
+			// Add the blog description for the home/front page.
+			$site_description = get_bloginfo( 'description', 'display' );
+			if ( $site_description && ( is_home() || is_front_page() ) )
+				echo " | $site_description";
+
+			// Add a page number if necessary:
+			if ( $paged >= 2 || $page >= 2 )
+				echo ' | ' . sprintf( __( 'Page %s', 'twentyten' ), max( $paged, $page ) );
+
+			?></title>
+			
+			
+			<link rel="profile" href="http://gmpg.org/xfn/11" />
+			<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+			<?php
+				/* We add some JavaScript to pages with the comment form
+				 * to support sites with threaded comments (when in use).
+				 */
+				if ( is_singular() && get_option( 'thread_comments' ) )
+					wp_enqueue_script( 'comment-reply' );
+
+				/* Always have wp_head() just before the closing </head>
+				 * tag of your theme, or you will break many plugins, which
+				 * generally use this hook to add elements to <head> such
+				 * as styles, scripts, and meta tags.
+				 */
+				wp_head();
+			?>
+			<style type="text/css" media="screen">
+				html { margin-top: 0px !important; }
+				* html body { margin-top: 0px !important; }
+			</style>
+			</head>
+			
+			
+			<link href="/stylesheets/style.css?1318789477" media="screen" rel="stylesheet" type="text/css" />
+
+			<!-- Include jQuery first. -->
+			<script type="text/javascript" src="/javascripts/jquery-1.3.2.min.js"></script>
+
+		    <!-- Include the below script, Copyright 2010, Brandon Aaron (http://brandonaaron.net/) for scrollwheel support. -->
+		    <script type="text/javascript" src="/javascripts/jquery-mousewheel/jquery.mousewheel.min.js"></script>
+
+			<link rel="stylesheet" type="text/css" href="/stylesheets/slidedeck.skin.css" media="screen" />
+		    <!-- Styles for the skin that only load for Internet Explorer -->
+		    <!--[if lte IE 8]>
+		    <link rel="stylesheet" type="text/css" href="/stylesheets/slidedeck.skin.ie.css" media="screen,handheld" />
+		    <![endif]-->
+
+		    <!-- Include the SlideDeck jQuery script. -->
+			<script type="text/javascript" src="/javascripts/slidedeck.jquery.lite.pack.js"></script>
+
+		    <style type="text/css">
+		        #slidedeck_frame {
+		            width: 901px;
+		            height: 300px;
+		        }        
+		    </style>
+		</head>
+		<body>
+
+			<div id="banner">
+
+
+				<div id="navigation">
+					<ul>
+						<li><a href="/">Home</a></li>
+						<li><a href="/project">Projects</a></li>
+						<li><a href="/blog">Blog</a></li>
+						<li><a href="/contact">Contact</a></li>
+
+					</ul>
+				</div>
+				<div id="social">
+					<ul>
+						<li><a href="http://feeds.feedburner.com/8bit/scraps" target="_blank"><img alt="Rss" height="27" src="/images/rss.png?1318360266" width="27" /></a></li>
+						<li><a href="http://twitter.com/#!/8bitscraps" target="_blank"><img alt="Twitter" height="40" src="/images/twitter.png?1318359914" style="margin-top:-5px;" width="40" /></a></li>
+
+						<li><a href="https://github.com/bitscraps" target="_blank"><img alt="Github_logo" height="30" src="/images/github_logo.png?1318359469" width="30" /></a></li>
+
+					</ul>
+				</div>
+
+		  	</div>
+
+			<div id="header"><img alt="8bitscraps" src="/images/8bitscraps.png?1318357012" /></div>
+
+			<hr>
+
+			<div id="content-container">
