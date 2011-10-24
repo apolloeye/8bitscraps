7a901d9a47d07107a45fb96be9b3482362433501
Graham Hadgraft <graham@avunit.com>
2011-10-24 16:03:52 +0100

    override default search box

---
+++ public/blog/wp-content/themes/twentyten/searchform.php
@@ -0,0 +1,6 @@
+<form role="search" method="get" id="searchform" action="http://www.8bitscraps.com/blog/" >
+	<div>
+	<input type="search" value="" name="s" id="s" />
+	<input type="submit" id="searchsubmit" value="Search" />
+	</div>
+</form>
