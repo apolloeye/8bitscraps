4c3323cf59af62d47236a2129138f3f44aeee0e6
Graham Hadgraft <graham@avunit.com>
2011-10-25 13:21:29 +0100

    new load tweet/commit pages should have no layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,5 +4,13 @@ class HomeController < ApplicationController
 
   def contact
   end
+ 
+  def latest_commits
+  	render :layout => false
+  end
+  
+  def latest_tweets 
+  	render :layout => false
+  end
 
 end
