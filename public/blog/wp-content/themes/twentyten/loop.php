2b505430ec7ed028821b553bde1e4838d6afc048
Graham Hadgraft <graham@avunit.com>
2011-11-02 13:18:18 +0000

    updated blog loop format

--- public/blog/wp-content/themes/twentyten/loop.php
+++ public/blog/wp-content/themes/twentyten/loop.php
@@ -128,46 +128,48 @@
 
 	<?php else : ?>
 		<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			
+		
 			<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyten' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+				<?php $lead = get_post_meta($post->ID, "summary_thumbnail", true);?>
+					<?php if($lead !== '') { ?>
+						<div class="summary-image">
+						<img src="<?php echo $lead ?>"  />  
 
-			<div class="entry-meta">
-				<?php twentyten_posted_on(); ?>
-			</div><!-- .entry-meta -->
+				</div>
+				<?php } ?>
+			
+				<div style="float:left;
+				<?php if($lead !== '') { ?>margin-left:20px;
+				
+					<?php } ?>
+					">
+					<img src="/images/calendar.png">
+				</div>
+				<div style="float:left;margin-left:10px;padding-top:3px;">
+					<?php the_time('l, F jS, Y') ?>					</div>
+			<br><br>
+			<!-- .entry-meta -->
 
 	<?php if ( is_archive() || is_search() ) : // Only display excerpts for archives and search. ?>
-			<div class="entry-summary">
+			<div class="entry-summary" 	<?php if($lead !== '') { ?>
+			style="margin-left:160px;">
+				<?php } ?>
 				<?php the_excerpt(); ?>
 			</div><!-- .entry-summary -->
 	<?php else : ?>
-			<div class="entry-content">
-				<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentyten' ) ); ?>
-				<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'twentyten' ), 'after' => '</div>' ) ); ?>
+			<div class="entry-content" 	<?php if($lead !== '') { ?>
+			style="margin-left:160px;">
+				<?php } ?>
+					<?php the_excerpt(); ?>
 			</div><!-- .entry-content -->
 	<?php endif; ?>
 
-			<div class="entry-utility">
-				<?php if ( count( get_the_category() ) ) : ?>
-					<span class="cat-links">
-						<?php printf( __( '<span class="%1$s">Posted in</span> %2$s', 'twentyten' ), 'entry-utility-prep entry-utility-prep-cat-links', get_the_category_list( ', ' ) ); ?>
-					</span>
-					<span class="meta-sep">|</span>
-				<?php endif; ?>
-				<?php
-					$tags_list = get_the_tag_list( '', ', ' );
-					if ( $tags_list ):
-				?>
-					<span class="tag-links">
-						<?php printf( __( '<span class="%1$s">Tagged</span> %2$s', 'twentyten' ), 'entry-utility-prep entry-utility-prep-tag-links', $tags_list ); ?>
-					</span>
-					<span class="meta-sep">|</span>
-				<?php endif; ?>
-				<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'twentyten' ), __( '1 Comment', 'twentyten' ), __( '% Comments', 'twentyten' ) ); ?></span>
-				<?php edit_post_link( __( 'Edit', 'twentyten' ), '<span class="meta-sep">|</span> <span class="edit-link">', '</span>' ); ?>
-			</div><!-- .entry-utility -->
+			<!-- .entry-utility -->
 		</div><!-- #post-## -->
 
 		<?php comments_template( '', true ); ?>
-
+<hr style="width:660px;">
 	<?php endif; // This was the if statement that broke the loop into three parts based on categories. ?>
 
 <?php endwhile; // End the loop. Whew. ?>
