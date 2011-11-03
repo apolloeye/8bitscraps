5f987a958cdc2f14b85d2dcbbb43f802a5db8edc
Graham Hadgraft <graham@avunit.com>
2011-11-03 20:35:28 +0000

    populated footer

--- public/blog/wp-content/themes/twentyten/footer.php
+++ public/blog/wp-content/themes/twentyten/footer.php
@@ -2,10 +2,29 @@
 	<div style="clear:both;"></div>
 	
 	<div id="footer">
-		<div id="left-column"></div>
-		<div id="right-column"></div>
+		<div id="footer-content">
+			<div id="left-column">
+				<p>
+					<img src="/images/graham.jpg" style="float:left;margin-right:20px;"/>
+					8 bit scraps has been created by Graham Hadgraft a web developer from Ipswich. Graham Hadgraft has 10 Years programming experience in various languages.</p>
+
+				<p>The aim of 8 bit scraps is to provide programming tips for Ruby on Rails, PHP, Perl and any other programming info that people may find useful. As well as reviews of programming resources such as books, software and websites.</p>
+			</div>
+			<div id="right-column">
+				<h3>Why not follow 8-Bit Scraps with</h3>
+				<ul>
+					<li><a href="http://feeds.feedburner.com/8bit/scraps" target="_blank">RSS</a></li>
+					<li><a href="http://twitter.com/#!/8bitscraps" target="_blank">Twitter</a></li>
+				
+					<li><a href="https://github.com/bitscraps" target="_blank">Github</a></li>
+					<li><a href="https://www.8bitscraps.co.uk/blog" target="_blank">Wordpress Blog</a></li>
+							
+				</ul>
+			</div>
+		</div>
 		<div id="copyright"><center><p style="align:center">&copy; 2011  Graham Hadgraft</p></center>
 	</div>
+	
 <?php wp_footer(); ?>	
 </body>
 </html>
