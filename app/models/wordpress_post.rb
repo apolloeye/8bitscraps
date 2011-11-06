92bd549af4aeef5ad1acf1622f924c97946da4c0
Graham Hadgraft <graham@avunit.com>
2011-11-06 20:51:35 +0000

    New definitions to provide access to meta post data using the wordpress_meta model

--- app/models/wordpress_post.rb
+++ app/models/wordpress_post.rb
@@ -3,5 +3,23 @@ class WordpressPost < ActiveRecord::Base
 	establish_connection :wordpress
   def self.table_name() "wp_posts" end
 
-	
+  def thumbnail
+  	@thumb = WordpressMeta.find(:first, :conditions => ["post_id = ? AND meta_key = ?", self.ID, "summary_thumbnail"]);
+  	if @thumb 
+  		return @thumb.meta_value
+  		
+  	else
+  		return nil
+  	end
+  end
+  
+  def permalink
+  	@thumb = WordpressMeta.find(:first, :conditions => ["post_id = ? AND meta_key = ?", self.ID, "permalink"]);
+  	if @thumb 
+  		return @thumb.meta_value
+  		
+  	else
+  		return nil
+  	end
+  end
 end
